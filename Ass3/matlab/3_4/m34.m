02078a7e9d3df17e8386b65cecb5aee475128ee9
Stefan Nöhmer <s.noehmer@gmail.com>
2012-01-27 17:09:37 +0100

    added search for parameters of problem 3.4, best SNR=3.81dB at N=236, mu=0.00205, delta=16

--- Ass3/matlab/3_4/m34.m
+++ Ass3/matlab/3_4/m34.m
@@ -1,21 +1,43 @@
 %load('speech_signals.mat');
 
-
+% parameters tom
 N = 100;
 mu = 0.005;
 
-delta =7;
+delta = 7;
 
+mmse_min = inf;
+N_min = 0;
+mu_min = 0;
+delta_min = 0;
 
 d = dtmfs(:);
-x = [zeros(delta,1);d(1:end-delta)];
+
+for N = 235:1:237
+    for mu = 0.0019:0.00005:0.0021
+        for delta = 15:1:17
+
+            x = [zeros(delta,1);d(1:end-delta)];
 
 
-[ y, e, c] = nlms2( x, d, N, mu);
+            [ y, e, c] = nlms2( x, d, N, mu);
 
-%e = d;
+            %e = d;
 
-MMSQE = sum((e - clean).^2);
-SNR = 10*log(sum((clean-mean(clean)).^2)/MMSQE);
-disp(num2str(delta));
+            MMSQE = sum((e - clean).^2);
+
+            if MMSQE < mmse_min
+                mmse_min = MMSQE;
+                N_min = N;
+                mu_min = mu;
+                delta_min = delta;
+            end
+            
+        end
+    end
+end
+
+SNR = 10*log(sum((clean-mean(clean)).^2)/mmse_min);
+
+disp(['optimal parameters: N=', num2str(N_min), ', mu=', num2str(mu_min), ', delta=', num2str(delta_min)]);
 disp(['SNR: ', num2str(SNR), 'dB']);
